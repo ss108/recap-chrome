24b0479825275f2264a2d8b1ac045efb3cf480c3
ss108 <samier_saeed@protonmail.com>
2022-02-06 20:01:57 -0500

    need to figure out ts compile in jest workflow

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,34 @@
+// module.exports = {
+//   moduleNameMapper: {
+//     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+//       '<rootDir>/__mocks__/fileMock.js',
+//     '\\.(css|less)$': '<rootDir>/src/assets/css/',
+//   },
+//   setupFilesAfterEnv: ['./jest.setup.js'],
+//   testPathIgnorePatterns: [
+//     '<rootDir>/src/assets/',
+//     '<rootDir>/__tests__/appellate/mocks',
+//     '<rootDir>/__tests__/district/mocks',
+//     '<rootDir>/__tests__/RecapSpec.js',
+//   ],
+// };
+
 module.exports = {
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/src/assets/css/',
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.ts?$": "ts-jest"
   },
-  setupFilesAfterEnv: ['./jest.setup.js'],
-  testPathIgnorePatterns: [
-    '<rootDir>/src/assets/',
-    '<rootDir>/__tests__/appellate/mocks',
-    '<rootDir>/__tests__/district/mocks',
-    '<rootDir>/__tests__/RecapSpec.js',
-  ],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.js',
+    // '<rootDir>/__tests__/RecapSpec.js',
+    '<rootDir>/__tests__/pacer.spec.ts',],
+  testPathIgnorePatterns: ['node_modules', '.cache', 'dist'],
+  transformIgnorePatterns: ['node_modules/'],
+  globals: {
+    chrome: true,
+  },
+  coverageReporters: ['text', 'html'],
+  coverageDirectory: './coverage/'
 };
